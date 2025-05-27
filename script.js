98c0174b6842ea6ca74271362cae247efa99bb65
Rythem aggarwal <115411745+RYTHEMAGGARWAL@users.noreply.github.com>
2025-05-27 12:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function highlight() {
-    //Write your code here
+const link=document.getElementById("link");
+  const str=document.querySelectorAll("strong");
+  function highlight(){
+    str.forEach(el=>{
+    el.style.color='rgb(0, 128, 0)';
+  });
+  }
+  function return_normal(){
+    str.forEach(el=>{
+    el.style.color="rgb(0, 0, 0)";
+  });
+  }
 
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
-    
-}
